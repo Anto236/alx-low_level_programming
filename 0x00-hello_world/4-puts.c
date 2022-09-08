c2053489191af61724e086ea3cc9c47c1a5ba7eb
Anto <Antonymbugua1237@gmail.com>
2022-09-08 14:37:36 -0700

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,12 @@
-puts("\"Programming is like building a multilingual puzzle");
-
-return (0);
-
+#include <stio.h>
+/**
+ * main - prints out a string to std output
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0)
+}
