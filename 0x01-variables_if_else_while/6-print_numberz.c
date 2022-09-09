8ac9dbbed4b35ce2f56c5e7c6ef9b589ac7aa1d0
Anto <Antonymbugua1237@gmail.com>
2022-09-09 15:04:54 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single numbers from 0 to 10 without char
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	{
+	putchar("\n");
+	return (0);
+	}
+}
