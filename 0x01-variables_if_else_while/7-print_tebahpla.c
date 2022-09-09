bcb4ad0b2bf58f5c2be8ab7408b2560e8d0b59c9
Anto <Antonymbugua1237@gmail.com>
2022-09-09 15:25:57 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - prints the lower case alphabet in reverse
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	{
+	putchar('\n');
+	return (0);
+	}
+}
