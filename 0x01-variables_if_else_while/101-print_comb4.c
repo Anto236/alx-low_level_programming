e68e6722a46e405e6d64813b99e133c363c781cf
Anto <Antonymbugua1237@gmail.com>
2022-09-11 07:52:25 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do...

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,16 +8,16 @@ int main(void)
 {
 	int i, e, g;
 
-	i = 48
-	e = 48
-	g = 48
+	i = 48;
+	e = 48;
+	g = 48;
 
 	while (e < 58)
 	{
-		i = 48
+		i = 48;
 		while (i < 58)
 		{
-			g = 48
+			g = 48;
 			while (g < 58)
 			{
 				if (i != e && i != g && e < i && i < g)
