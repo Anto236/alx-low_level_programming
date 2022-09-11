69b8a50464fb3c5d09a2dc274cee8740f99b3f9e
Anto <Antonymbugua1237@gmail.com>
2022-09-11 07:49:04 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do...

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+/**
+ * main - Prints numbers between "012" to 789
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i, e, g;
+
+	i = 48
+	e = 48
+	g = 48
+
+	while (e < 58)
+	{
+		i = 48
+		while (i < 58)
+		{
+			g = 48
+			while (g < 58)
+			{
+				if (i != e && i != g && e < i && i < g)
+				{
+					putchar(e);
+					putchar(i);
+					putchar(g);
+					if (i == 57 && e == 56 && g == 58)
+					{
+						break;
+					}
+					putchar(',');
+					putchar(' ');
+				}
+				g++
+			}
+			i++
+		}
+		e++
+	}
+	putchar('\n')
+	return (0)
+}
