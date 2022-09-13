78fbc24d3a4be532ce6a951ba12d9fd664b72cba
Anto <Antonymbugua1237@gmail.com>
2022-09-13 15:48:02 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,26 +7,18 @@
  */
 void print_to_98(int n)
 {
-	int i, j;
-
-	if (n <= 98)
+	if (n > 98)
 	{
-		for (i = n; i <= 98; i++)
+		for (; n > 98; n--)
 		{
-			if (i != 98)
-			{
-				printf("%d, ", i);
-			}
+			printf("%d, ", n);
 		}
 	}
-	if (n >= 98)
+	else if (n < 98)
 	{
-		for (j = n; j >= 98; j--)
+		for (; n < 98; n++)
 		{
-			if (j != 98)
-			{
-				printf("%d, ", j);
-			}
+			printf("%d, ", n);
 		}
 	}
 	printf("%d\n", n);
