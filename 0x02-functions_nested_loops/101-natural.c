94e743b68210cbf1dad492156e82e4f1e8aed467
Anto <Antonymbugua1237@gmail.com>
2022-09-14 07:27:41 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,11 +7,11 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 1; i < 1024; a++)
+	for (i = 1; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
 			j += i
 	}
-	printf("d%\n", b);
+	printf("d%\n", j);
 	return (0);
 }
