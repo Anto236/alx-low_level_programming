53870fa2c14c0c1899308ed85454de30d195d1af
Anto <Antonymbugua1237@gmail.com>
2022-09-19 13:02:37 -0700

    6. Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string
+ * @str: string to be printed
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (i >= 0)
+	{
+		if (str[i] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+		i++;
+	}
+}
