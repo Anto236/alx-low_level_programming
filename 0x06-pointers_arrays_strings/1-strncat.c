3cea29490f07bfa23e53683df3ad0d9bc9954449
Anto <Antonymbugua1237@gmail.com>
2022-09-21 22:16:35 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -3,6 +3,7 @@
  * _strcat - concatenates two strings
  * @dest: string to append to
  * @src: sting to be added
+ * @n: amount of bytes used from src
  * Return: Returns a pointer to the resulting string
  */
 char *_strncat(char *dest, char *src, int n)
