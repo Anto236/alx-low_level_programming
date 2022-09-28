a92a3dcb8fe97c74e5788270b0bda1277450516a
Anto <Antonymbugua1237@gmail.com>
2022-09-28 05:58:16 -0700

    3. You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * factorial - finds factorial
+ * @n: number of times for factorial
+ * Return: factorial of the number
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
