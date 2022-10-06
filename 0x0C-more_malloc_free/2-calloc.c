a078d1b0eb3e0a90635405f57392e55be7bd234a
Anto <Antonymbugua1237@gmail.com>
2022-10-06 12:02:19 -0700

    array memory

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: elements in an array
+ * @size: bytes in elements of the array
+ * Return: pointer to an element
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i, j;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	j = size * nmemb;
+	p = malloc(j);
+	for (i = 0; i < j; i++)
+		p[i] = 0;
+	return (p);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -1,4 +1,4 @@
 #0x0C. C - More malloc, free
 0. Trust no one - function that allocates memory using malloc, returns a pointer to the allocated memory and if malloc fails, it causes normal termination with status value of 98
-1. string _nconcat - concate two strings. return pointer to newly allocated memory.
-
+1. string _nconcat - concate two strings. return pointer to newly allocated memory
+2. _calloc - allocate memory for an array
