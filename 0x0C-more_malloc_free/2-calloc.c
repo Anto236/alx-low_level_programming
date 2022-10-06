5b60f213baa3600b0bbe8d1b15f6daa3740fb84c
Anto <Antonymbugua1237@gmail.com>
2022-10-06 12:10:08 -0700

    mem alloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	p = malloc(nmemb * size)
+	p = malloc(nmemb * size);
 	for (i = 0; i < (nmemb * size); i++)
 		p[i] = 0;
 	return (p);
