ef8bd142e0f558779678d5ef67813af4c9bdcfd7
Anto <Antonymbugua1237@gmail.com>
2022-10-06 13:04:29 -0700

    array of int

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * array_range - create an array of integers
+ * @min: minimum range
+ * @max: maximum range
+ * Return: pointer to an arrray of integers
+ */
+int *array_range(int min, int max)
+{
+	int *p;
+	int i, j;
+
+	if (min > max)
+		return (NULL);
+
+	j = max - min;
+	p = malloc(sizeof(int) * j);
+
+	if (p == NULL)
+		return (NULL);
+	for (i = 0; i < j; i++)
+		p[i] = min++;
+	return (p);
+}
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -2,3 +2,4 @@
 0. Trust no one - function that allocates memory using malloc, returns a pointer to the allocated memory and if malloc fails, it causes normal termination with status value of 98
 1. string _nconcat - concate two strings. return pointer to newly allocated memory
 2. _calloc - allocate memory for an array
+3. array _range - creates an array of integers
