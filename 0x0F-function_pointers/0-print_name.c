f33260a0f8b280d9e505d08e96c54f2ebbdbc233
Anto236 <Antonymbugua1237@gmail.com>
2022-10-12 08:51:07 -0700

    lets go

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,8 @@
 #include "function_pointers.h"
 /**
- *
+ * print_name - prints the name when function pointer is declared
+ * @name: name to be printed
+ * @f: pointer to a function
  */
 void print_name(char *name, void (*f)(char *))
 {
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+#0x0F. C - Function pointers
+| FILENAME | DESCRIPTION |
+| '0-print_name.c'| print name |
