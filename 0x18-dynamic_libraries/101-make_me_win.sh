7f03e75bfc7a9f24fc3b022ae1467a6ead8c7cd2
Anto236 <Antonymbugua1237@gmail.com>
2022-12-13 11:08:58 +0000

    shared

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P https://github.com/Anto236/alx-low_level_programming/main/0x18-dynamic_libraries/libtest.so
+wget -P /tmp/ https://github.com/Anto236/alx-low_level_programming/0x18-dynamic_libraries/libtest.so
 export LD_PRELOAD=/tmp/libtest.so
