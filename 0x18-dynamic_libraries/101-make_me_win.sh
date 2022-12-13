604c01665563a96b94566f40690195b8f289e072
Anto236 <Antonymbugua1237@gmail.com>
2022-12-13 11:16:17 +0000

    shared

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp/https://github.com/Anto236/alx-low_level_programming/blob/11345b83fce94a585157cec9a474fba1757ee057/0x18-dynamic_libraries/libtest.so
-export LD_PRELOAD=/tmp/libtest.s
+wget -P /tmp/https://github.com/Anto236/alx-low_level_programming/master/0x18-dynamic_libraries/libtest.so
+export LD_PRELOAD=/tmp/libtest.so
